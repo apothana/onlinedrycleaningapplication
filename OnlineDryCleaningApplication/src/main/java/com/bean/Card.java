c0e7bf7e33952ec3373d87c0e18676ff983955d6
ARUN NANI <ARUN NANI@DESKTOP-VGRSRNU>
2021-06-20 18:15:30 +0530

    Project

---
+++ OnlineDryCleaningApplication/src/main/java/com/bean/Card.java
@@ -0,0 +1,70 @@
+package com.bean;
+import java.time.LocalDate;
+
+import javax.persistence.Embeddable;
+import javax.validation.Valid;
+
+import com.sun.istack.NotNull;
+
+@Embeddable
+public class Card {
+	
+	@NotNull
+	private String cardName;
+	private String cardNumber;
+	@NotNull
+	private LocalDate expiryDate;
+	private String bankName;
+
+	public Card() {
+		super();
+	}
+
+	public Card(@NotNull String cardName, String cardNumber,@NotNull LocalDate expiryDate, String bankName) {
+		super();
+		this.cardName = cardName;
+		this.cardNumber = cardNumber;
+		this.expiryDate = expiryDate;
+		this.bankName = bankName;
+	}
+
+	
+	public String getCardName() {
+		return cardName;
+	}
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public LocalDate getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(LocalDate expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	@Override
+	public String toString() {
+		return "Card [id="  + ", cardName=" + cardName + ", cardNumber=" + cardNumber + ", expiryDate=" + expiryDate
+				+ ", bankName=" + bankName + "]";
+	}
+
+}
